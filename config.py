9cf6dd0b6d826861b67e675035c497dc4c5acb6d
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2020-05-17 14:55:53 +0200

    .

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 DEBUG = False
 TESTING = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///'
@@ -8,7 +6,6 @@ SQLALCHEMY_ECHO = True
 SECRET_KEY = 'acU7/<}Jr\(!m,JX'
 SESSION_COOKIE_HTTPONLY = False
 SESSION_TYPE = 'redis'
-QUERY_MAX_RESULT_COUNT = 1000
 STRIPE_URL = 'https://api.stripe.com'
 STRIPE_KEY = 'sk_test_KGm6pfEhnrCPDV235bUMmUWV'
 SMTP_HOST = 'smtp.gmail.com'
@@ -16,8 +13,3 @@ SMTP_PORT = 465
 SMTP_USERNAME = 'poussy.magnette@gmail.com'
 SMTP_PASSWORD = 'fwudamcgudlkenjz'
 BASE_URL = 'http://localhost:5000'
-GOOGLE_CLIENT_ID = '588518739043-oe363kajcna7evu7m96f6rdikkhqmoee.apps.googleusercontent.com'
-GOOGLE_APPLICATION_CREDENTIALS = str(Path.home() / 'dip-ops-6a5560cff052.json')
-DARTS_URL = 'http://localhost:8080'
-DARTS_USERNAME = 'darts'
-DARTS_PASSWORD = 'no1ere4ever'
