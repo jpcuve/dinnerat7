c19860a7048c4e600754b9507a13e34a52970f2e
jpc <jpcuvelliez@gmail.com>
2020-05-17 18:41:47 +0200

    .

--- dinnerat7/api/api_web.py
+++ dinnerat7/api/api_web.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 from dinnerat7 import find_dating_events
 
@@ -9,3 +9,28 @@ bp = Blueprint('api_web', __name__, url_prefix='/api/web')
 def api_default():
     dating_events = find_dating_events()
     return render_template('index.html', events=dating_events)
+
+
+@bp.route('/auth')
+def api_auth():
+    return render_template(
+        'auth.html',
+        firebase_api_key=current_app.config['FIREBASE_API_KEY'],
+        firebase_auth_domain=current_app.config['FIREBASE_AUTH_DOMAIN'],
+        firebase_database_url=current_app.config['FIREBASE_DATABASE_URL'],
+        firebase_project_id=current_app.config['FIREBASE_PROJECT_ID'],
+        firebase_storage_bucket=current_app.config['FIREBASE_STORAGE_BUCKET'],
+        firebase_messaging_sender_id=current_app.config['FIREBASE_MESSAGING_SENDER_ID'],
+        firebase_app_id=current_app.config['FIREBASE_APP_ID'],
+        firebase_measurement_id=current_app.config['FIREBASE_MEASUREMENT_ID']
+    )
+
+
+@bp.route('/privacy-policy')
+def api_privacy_policy():
+    return render_template('privacy-policy.html')
+
+
+@bp.route('/terms-of-service')
+def api_terms_of_service():
+    return render_template('terms-of-service.html')
